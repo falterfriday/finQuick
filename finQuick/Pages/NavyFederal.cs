f8a7bda664df66752758976f2610df5b93bf2123
Patrick Todd <patrick.todd@reisystems.com>
2017-10-06 23:20:49 -0400

    added page object

---
+++ finQuick/Pages/NavyFederal.cs
@@ -0,0 +1,24 @@
+﻿using finQuick.Utilities;
+using OpenQA.Selenium;
+using Yarr = finQuick.Secrets.HereBeDragons;
+
+namespace finQuick.Pages
+{
+    class NavyFederal
+    {
+        private static IWebElement UsernameInput => Driver.Instance.FindElement(By.Id("user"));
+
+        private static IWebElement PasswordInput => Driver.Instance.FindElement(By.Id("password"));
+
+        private static IWebElement loginButton => Driver.Instance.FindElement(By.Name("signin"));
+
+
+
+
+        public static void Login()
+        {
+            UsernameInput.SendKeys(Yarr.NavyFederal[0]);
+            PasswordInput.SendKeys(Yarr.NavyFederal[1]);
+        }
+    }
+}
