a26bf37ec0d6a6670b5fd2a1eefa7e7c970f1750
Patrick Todd <patrick.todd@reisystems.com>
2017-10-06 23:20:16 -0400

    combined page objects for same site

---
+++ finQuick/Pages/BankOfAmerica.cs
@@ -0,0 +1,40 @@
+﻿using finQuick.Utilities;
+using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+using Yarr = finQuick.Secrets.HereBeDragons;
+
+namespace finQuick.Pages
+{
+    public static class BankOfAmerica
+    {
+        private static IWebElement LoginUsernameInput => Driver.Instance.FindElement(By.Id("onlineId1"));
+
+        private static IWebElement LoginPasswordInput => Driver.Instance.FindElement(By.Id("passcode1"));
+
+        private static IWebElement LoginButton => Driver.Instance.FindElement(By.Id("hp-sign-in-btn"));
+
+        private static ReadOnlyCollection<IWebElement> CardBalances => Driver.Instance.FindElements(By.ClassName("balanceValue"));
+
+        private static IWebElement LogOutLink => Driver.Instance.FindElement(By.LinkText("Sign Out"));
+
+        public static void Login()
+        {
+            LoginUsernameInput.SendKeys(Yarr.BankOfAmerica[0]);
+            Driver.Instance.Wait(0.5);
+            LoginPasswordInput.SendKeys(Yarr.BankOfAmerica[1]);
+            LoginButton.Click();
+            Driver.Instance.Wait(2);
+        }
+
+        public static ReadOnlyCollection<IWebElement> MainPageInfo()
+        {
+            return CardBalances;
+        }
+
+        public static void LogOut()
+        {
+            LogOutLink.Click();
+            Driver.Instance.Wait(1);
+        }
+    }
+}
